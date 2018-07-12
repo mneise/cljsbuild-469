fc13585484518123e8f33008f0b2d38940490598
Maria Geller <maria.geller2@gmail.com>
2018-07-12 10:16:36 +0200

    Update project name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject test-fipp "0.1.0-SNAPSHOT"
+(defproject cljsbuild-469 "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [fipp "0.6.6"]]
